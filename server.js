3e117266c020811679475a9a1d0465161ad654af
Cipee <mike15intanno@gmail.com>
2023-10-27 17:18:33 +0200

    make it better

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-const express = require("")
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+
+})
+
+
+app.listen(process.env.PORT || 3000, (req, res) => {
+    console.log("Server start")
+})
